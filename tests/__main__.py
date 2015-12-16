d33210b2f3f16fd4f34d8dd1838b815b0b1743fe
Johann Schmitz <johann@j-schmitz.net>
2015-12-16 17:15:28 +0100

    Disable logging for test

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 
 try:
-    import unittest2 as unittest
+	import unittest2 as unittest
 except ImportError:
-    import unittest
+	import unittest
 
 from response import *
 from messages import *
 from cube import *
 from util import *
 
+import logging
+logging.basicConfig(
+	level=logging.CRITICAL,
+	format='%(asctime)s %(levelname)-7s %(message)s',
+)
+
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
