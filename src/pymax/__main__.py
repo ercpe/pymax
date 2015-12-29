90acc5ea14689e491ec552a0c937d0efd9282262
Johann Schmitz <johann@j-schmitz.net>
2015-12-29 12:54:03 +0100

    Allow passing a host as positional argument to skip discovery

--- src/pymax/__main__.py
+++ src/pymax/__main__.py
@@ -13,29 +13,32 @@ if __name__ == "__main__": # pragma: no cover
 	parser = ArgumentParser()
 	parser.add_argument('-v', '--verbose', action="count", default=1)
 	parser.add_argument('-s', '--serial', help='Query cube with serial')
+	parser.add_argument('host', nargs='?')
 
 	args = parser.parse_args()
 
 	logging.basicConfig(level=logging.FATAL - (10 * args.verbose), format='%(asctime)s %(levelname)-7s %(message)s')
 
-	serial = args.serial
+	host = args.host
+	if not host:
+		serial = args.serial
+		if not serial:
+			response = Discovery().discover()
+			if response:
+				serial = response.serial
+			else:
+				print("No cubes found.")
+				sys.exit(1)
 
-	if not serial:
-		response = Discovery().discover()
-		if response:
-			serial = response.serial
-		else:
-			print("No cubes found.")
+		try:
+			net_cfg_response = Discovery().discover(cube_serial=serial, discovery_type=Discovery.DISCOVERY_TYPE_NETWORK_CONFIG)
+			print("Discovered cube: %s" % net_cfg_response)
+			host = net_cfg_response.ip_address
+		except socket.timeout as st:
+			print("Could not find cube '%s': %s" % (serial, st))
 			sys.exit(1)
 
-	try:
-		net_cfg_response = Discovery().discover(cube_serial=serial, discovery_type=Discovery.DISCOVERY_TYPE_NETWORK_CONFIG)
-		print("Discovered cube: %s" % net_cfg_response)
-	except socket.timeout as st:
-		print("Could not find cube '%s': %s" % (serial, st))
-		sys.exit(1)
-
-	with Cube(net_cfg_response) as cube:
+	with Cube(host) as cube:
 		print("")
 		print(cube.info)
 
