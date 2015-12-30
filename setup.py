49925cd8daaba866e4230a07c9fe2654170408a3
Johann Schmitz <johann@j-schmitz.net>
2015-12-30 06:33:04 +0100

    Release 0.2

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 
 setup(
 	name='pymax',
-	version='0.2',
+	version='0.3',
 	description='Python implementation of the ELV MAX! Cube API',
 	author='Johann Schmitz',
 	author_email='johann@j-schmitz.net',
 	url='https://ercpe.de/projects/pymax',
-	download_url='https://github.com/ercpe/pymax/tarball/0.2',
+	download_url='https://github.com/ercpe/pymax/tarball/0.3',
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	include_package_data=True,
